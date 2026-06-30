0a8fbccea32bbfef9c3fedb2d8d156a4df92442f
Zach M <196665719+zachcmem@users.noreply.github.com>
2026-06-30 16:20:27 -0400

    connected delete api to dhasboard

--- src/app/api/goals/route.ts
+++ src/app/api/goals/route.ts
@@ -158,15 +158,15 @@ export async function POST(request: Request){
 //   ↓
 // Dashboard can display it
 
-export async function DELETE(request: Request){
-    try{
+// export async function DELETE(request: Request){
+//     try{
 
-    }
-    catch(error){
-        console.error("Failed to delete goal", error)
-        return NextResponse.json(
-            {error: "Failed to delete goal"},
-            {status: 500}
-        )
-    }
-}
+//     }
+//     catch(error){
+//         console.error("Failed to delete goal", error)
+//         return NextResponse.json(
+//             {error: "Failed to delete goal"},
+//             {status: 500}
+//         )
+//     }
+// }
