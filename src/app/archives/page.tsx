1fe0e1ad19fc4dfd16da793d80cc59d0c5bfec4e
Zach M <196665719+zachcmem@users.noreply.github.com>
2026-07-15 21:16:03 -0400

    added stats and archives page?

---
+++ src/app/archives/page.tsx
@@ -0,0 +1,17 @@
+export default function ArchivesPage(){
+    return(
+        <main className="mx-auto min-h-screen max-w-7xl p-8">
+            <section className="rounded-lg border border-teal-200 bg-neutral-900 p-6 shadow">
+                <h1 className="text-3xl font-bold text-teal-200">
+                    Archives
+                </h1>
+                <p className="mt-3 text-neutral-300">
+                    View goals that you have completed or removerd from your active dashboard
+                </p>
+                <p className="mt-6 text-neutral-300">
+                    Archived goals will appear here in a future update
+                </p>
+            </section>
+        </main>
+    )
+}
---
+++ src/app/stats/page.tsx
@@ -0,0 +1,23 @@
+
+// This page will eventually display information such as:
+
+// Total goals
+// Completed tasks
+// Overall progress
+// Weekly completion
+
+export default function StatsPage(){
+    return(
+        <main className="mx-auto min-h-screen max-w-7xl p-8">
+            <section className="rounded-lg border border-teal-200 bg-neutral-900 p-6 shadow">
+                <h1 className="">Stats</h1>
+                <p className="mt-3 text-netural-300">
+                    Review Your goals progress, task completion, and productivity trends
+                </p>
+                <p className="mt-6 text-sm text-neutral-500">
+                    Stats feature are coming soon
+                </p>
+            </section>
+        </main>
+    )
+}
