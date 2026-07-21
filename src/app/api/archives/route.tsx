1e88e68370da4c76f57ceef99afe68993136ab0c
Zach M <196665719+zachcmem@users.noreply.github.com>
2026-07-21 15:47:38 -0400

    added GET archirves endpoint for archive webpage

---
+++ src/app/api/archives/route.tsx
@@ -0,0 +1,34 @@
+
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { calculateProgress } from "@/lib/progress";
+
+export async function GET(){
+    try{
+        const archivedGoals = await prisma.goal.findMany({
+            where:{
+                archived: true
+            },
+            include:{
+                tasks: true
+            },
+            orderBy: {
+                archivedAt: "desc"
+            }
+        });
+
+        const archivedGoalsWithProgress = archivedGoals.map((goal)=> ({
+            ...goal,
+            progress: calculateProgress(goal.tasks)
+        }));
+        return NextResponse.json(archivedGoalsWithProgress);
+    }
+    catch(error){
+        console.error("Failed to fetch archived goals, ", error)
+
+        return NextResponse.json(
+            { error: "Failed to fetch archived goals"},
+            {status: 500}
+        );
+    }
+}
